f1593b753485192c6cd24fdabe2165eb10b6d845
Shariful Islam <shariful.islam.mec@gmail.com>
2022-05-20 20:07:57 +0300

    Exercise 1.6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Title = (props) => (
+  <div>{props.title}</div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const DisplayP = (props) => (
+<div> {props.textP} {props.counterP}</div>
+)
+
+const DisplayNtrl = (props) => (
+  <div> {props.textNtrl} {props.counterNtrl}</div>
+  )
+
+const DisplayN = (props) => (
+  <div> {props.textN} {props.counterN}</div>
+  )
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      counterP: 0,
+      counterNtrl: 0,
+      counterN:0
+    }
+  }
+
+  setValueP = (value) => {
+    return () => {
+      this.setState({ counterP: value })
+    }
+  }
+
+  setValueNtrl = (value) => {
+    return () => {
+      this.setState({ counterNtrl: value })
+    }
+  }
+
+  setValueN = (value) => {
+    return () => {
+      this.setState({ counterN: value })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+        <Title title="Give feedback" />
+        </h1>
+        <div>
+          <Button 
+            handleClick={this.setValueP(this.state.counterP + 1)} 
+            text="Positive" 
+          />
+          <Button 
+            handleClick={this.setValueNtrl(this.state.counterNtrl + 1)} 
+            text="Neutral" 
+          />
+          <Button 
+            handleClick={this.setValueN(this.state.counterN + 1)} 
+            text="Negative" 
+          />
+        </div>
+        <h2>
+          <Title title="Statistics"/>
+        </h2>
+        <div>
+        <DisplayP textP="Positive" counterP={this.state.counterP}/>
+        <DisplayNtrl textNtrl="Neutral" counterNtrl={this.state.counterNtrl}/>
+        <DisplayN textN="Negative" counterN={this.state.counterN}/>
+        </div>
+      </div>
+      
+    )
+  }
+}
+
+ReactDOM.render(
+<App />, 
+document.getElementById('root'))
