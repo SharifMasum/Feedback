34ac465fa7cd2bc056b9f3b471ee6169f43d8151
Shariful Islam <shariful.islam.mec@gmail.com>
2022-05-23 11:44:40 +0300

    Exercise 1.8

--- src/index.js
+++ src/index.js
@@ -11,25 +11,9 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const DisplayGd = (props) => (
-<div> {props.textP} {props.counterGd}</div>
+const Statistics = (props) => (
+  <div> {props.text} {props.counter}</div>
 )
-
-const DisplayNtrl = (props) => (
-  <div> {props.textNtrl} {props.counterNtrl}</div>
-  )
-
-const DisplayBd = (props) => (
-  <div> {props.textN} {props.counterBd}</div>
-  )
-
-const DisplayAvg = (props) => (
-  <div> {props.text} {props.counterAvg}</div>
-  )
-
-const DisplayGdPtg = (props) => (
-  <div> {props.text} {props.counterGdPtg}%</div>
-  )
 
 class App extends React.Component {
   constructor() {
@@ -37,9 +21,7 @@ class App extends React.Component {
     this.state = {
       counterGd: 0,
       counterNtrl: 0,
-      counterBd: 0,
-      counterAvg: 0,
-      counterGdPtg: 0
+      counterBd: 0
     }
   }
 
@@ -66,7 +48,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>
-        <Title title="Give feedback" />
+        <Title title="Give feedback"/>
         </h1>
         <div>
           <Button
@@ -86,16 +68,16 @@ class App extends React.Component {
           <Title title="Statistics"/>
         </h2>
         <div>
-        <DisplayGd textP="Good" counterGd={this.state.counterGd}/>
-        <DisplayNtrl textNtrl="Neutral" counterNtrl={this.state.counterNtrl}/>
-        <DisplayBd textN="Bad" counterBd={this.state.counterBd}/>
-        <DisplayAvg text="Average"
-                        counterAvg={((((this.state.counterGd)*1)+
+        <Statistics text="Good" counter={this.state.counterGd}/>
+        <Statistics text="Neutral" counter={this.state.counterNtrl}/>
+        <Statistics text="Bad" counter={this.state.counterBd}/>
+        <Statistics text="Average"
+                        counter={((((this.state.counterGd)*1)+
                         ((this.state.counterNtrl)*0)+((this.state.counterBd)*(-1)))/
                         ((this.state.counterGd)+(this.state.counterNtrl)+
                         (this.state.counterBd))||0).toFixed(2)}/>
-        <DisplayGdPtg text="Good percentage"
-                        counterGdPtg={(((this.state.counterGd)*100)/
+        <Statistics text="Good percentage"
+                        counter={(((this.state.counterGd)*100)/
                         ((this.state.counterGd)+(this.state.counterNtrl)+
                         (this.state.counterBd))||0).toFixed(2)}/>
         </div>
